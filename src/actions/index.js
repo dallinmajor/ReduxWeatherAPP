78d1b1d08113d7301caf978ea60230b98ef20cda
Dally Wally <dallin.watac.major@gmail.com>
2019-01-22 07:14:38 -0700

    added axios and created fetch weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
 const API_KEY = '577e37f5023a9a64efc45d0c0d29aeef';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FEATCH_WEATHER';
+
+export function fetchWeather(city) { 
+    const url = `${ROOT_URL}$q=${city},us`;
+    const request = axios.get(url);
+    
+    return {
+        type: FETCH_WEATHER,
+        payload: request 
+    }
+}
